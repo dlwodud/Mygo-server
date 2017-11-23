df869c491d897c74915d2d97326c0a7239abdb08
dlwodud <mail_look@naver.com>
2017-11-23 20:16:49 +0900

    version1.2

--- UEditFragment.php
+++ UEditFragment.php
@@ -9,9 +9,12 @@
 	$courseTitle = $_POST["courseTitle"];
 	$courseStroy = $_POST["courseStroy"];
     $courseWorry = $_POST["courseWorry"];
+    $targetGender = $_POST["targetGender"];
+    $targetAge = $_POST["targetAge"];
+    $timeToDelete = $_POST["timeToDelete"];
 
-	$statement = mysqli_prepare($con, "UPDATE COURSE  SET courseTitle = ?, courseStroy = ?, courseWorry = ?, courseDate1 = now() WHERE userID = ? AND courseID = ?");
-	mysqli_stmt_bind_param($statement, "ssssi",  $courseTitle, $courseStroy, $courseWorry, $userID, $courseID);
+	$statement = mysqli_prepare($con, "UPDATE COURSE  SET courseTitle = ?, courseStroy = ?, courseWorry = ?, courseDate1 = now(), targetGender = ?, targetAge = ? timeToDelete =? WHERE userID = ? AND courseID = ?");
+	mysqli_stmt_bind_param($statement, "sssssssi",  $courseTitle, $courseStroy, $courseWorry,$targetGender, $targetAge, $timeToDelete, $userID, $courseID);
 
 	$response = array();
 	$response["success"] =  mysqli_stmt_execute($statement);
--- UHom.php
+++ UHom.php
@@ -1,25 +1,35 @@
 <?php
+
 	include 'UDeleteAuto.php';
 
 	header("Content-Type: text/html; charset-UTF-8");
 	$con = mysqli_connect("localhost", "dlwodud200", "Lee7040!", "dlwodud200");
-
 	mysqli_set_charset($con,"utf8");
-
 	$courseWorry = $_GET["courseWorry"];
+	$targetGender = $_GET["targetGender"];
+	$targetAge = $_GET["targetAge"];
 	$userID = $_GET["userID"];
 
-	$result;
-	if($courseWorry == "*") {
-		$result = mysqli_query($con, "SELECT * FROM COURSE WHERE userID = '$userID'");
-	} 
-	else {
-		$result = mysqli_query($con,"SELECT * FROM COURSE WHERE courseWorry = '$courseWorry' AND userID = '$userID'");
+
+	$stmt = "SELECT * FROM COURSE WHERE userID = '$userID'";
+
+	if ($courseWorry  != "*") {
+		$stmt = $stmt . ", courseWorry = '$courseWorry'";
 	}
+
+	if ($targetGender  != "*") {
+		$stmt = $stmt . ", targetGender = '$targetGender'";
+	}
+
+	if ($targetAge  != "*") {
+		$stmt = $stmt . ", targetAge = '$targetAge'";
+	}
+
+	$result = mysqli_query($con, $stmt);
 
 	$response = array();
 	while($row = mysqli_fetch_array($result)){
-		array_push($response, array("courseID"=>$row[0],"userID"=>$row[1],"courseTitle"=>$row[2],"courseStroy"=>$row[3], "courseWorry"=>$row[4],"courseDate1"=>$row[5]));
+		array_push($response, array("courseID"=>$row[0],"userID"=>$row[1],"courseTitle"=>$row[2],"courseStroy"=>$row[3], "courseWorry"=>$row[4],"courseDate1"=>$row[5],"targetGender"=>$row[6],"targetAge"=>$row[7],"timeToDelete"=>$row[8]));
 	}
 	echo json_encode(array("response"=>$response), JSON_UNESCAPED_UNICODE);
 	mysqli_close($con);
